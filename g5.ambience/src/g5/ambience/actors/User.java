674d9d6c582965c2281199cc3c0bcc79ff5f545c
Ilya Radchenko <ilya@burstcreations.com>
2012-04-03 16:26:44 -0400

    User and Member classes created with vars and getter/setter functions.

---
+++ g5.ambience/src/g5/ambience/actors/User.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package g5.ambience.actors;
+
+/**
+ * @author ilya
+ *
+ */
+public class User {
+
+
+	private String username;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String password;
+	
+	
+	
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the first_name
+	 */
+	public String getFirst_name() {
+		return first_name;
+	}
+	/**
+	 * @param first_name the first_name to set
+	 */
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	/**
+	 * @return the last_name
+	 */
+	public String getLast_name() {
+		return last_name;
+	}
+	/**
+	 * @param last_name the last_name to set
+	 */
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+
+}
